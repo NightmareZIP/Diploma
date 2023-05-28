6d585b3b8a11a0fdce941e8711052d8b7cae30f8
Victor <31132241+NightmareZIP@users.noreply.github.com>
2023-05-28 14:33:22 +0300

    extra comments

--- project/worker/serializers.py
+++ project/worker/serializers.py
@@ -1,24 +1,36 @@
+# подключение библиотек
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Worker
 from company.models import Company
 from company.serializers import CompanySerializer
-
 from .utils import get_worker
 
 
 class WorkerCompanySerializer(serializers.ModelSerializer):
+    """Класс сериалайзер для применения при регистрации компании
+    Мы регистрируем компанию вместе с ее руководителем, и нужно работать одновременно с 2
+    моделями
+    """
+
+    # Указываем что одним из полей является сериалайзер компании, так мы сможем  получать ее данные
     company = CompanySerializer(required=True)
 
     class Meta:
+        """метаданные класса, указываем, вспомогательный класс основного класса
+        """
+        #Указываем основную модель для работы
         model = Worker
+        #Задаем поля, которые можно читать, но нельзя записывать
         read_only_fields = (
             "created_at",
             "head",
             # "company",
         ),
+        #Задаем набор полей, принимаемых сериалайзером
         fields = (
             'name',
+            'is_head',
             'last_name',
             'surname',
             'email',
@@ -31,20 +43,22 @@ class WorkerCompanySerializer(serializers.ModelSerializer):
         )
 
     def create(self, validated_data):
-        company = validated_data.pop('company')
+        """Создание записи в базе данных
 
+        Args:
+            validated_data: данные прошедшие валидацию
+        """
+        #Забираем данные касающиеся компании
+        company = validated_data.pop('company')
+        #создаем компанию
+        company = Company.objects.create(**company)
+        validated_data['company'] = company
+        validated_data['is_head'] = True
+
+        #Создаем сотрудника
         worker = Worker.objects.create(**validated_data)
         worker.save()
 
-        company['head'] = worker
-        company['created_by'] = worker
-
-        company = Company.objects.create(**company)
-        # company.created_by = worker
-        # company.head = worker
-        company.save()
-        worker.company = company
-        worker.save()
         return worker
 
 
@@ -60,8 +74,3 @@ class WorkerSerializer(serializers.ModelSerializer):
         ),
         fields = "__all__"
 
-    # def create(self, validated_data):
-    #     worker = Worker.object.create(**validated_data)
-    #     return worker
-
-    # def update(self, instance, validated_data)
